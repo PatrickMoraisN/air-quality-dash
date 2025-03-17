7b8c18b33fd7f28810ff950bc76839e2b1009e18
Patrick Morais <ppternunes@gmail.com>
2025-03-17 18:43:43 -0300

    feat(styles): creates first global and theme styles

---
+++ src/styles/themes.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  background: '#F5F5F5',
+  text: '#222',
+  primary: '#555555',
+  header: '#FFFFFF',
+}
+
+export const darkTheme = {
+  background: '#1E1E1E',
+  text: '#EAEAEA',
+  primary: '#2A2A2A',
+  header: '#333333',
+}
