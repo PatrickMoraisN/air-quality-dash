5939e7184bc8b2203b34847db750da57015ce80b
Patrick Morais <ppternunes@gmail.com>
2025-03-18 13:18:52 -0300

    feat(themes): add mode on consts

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,13 +1,17 @@
 export const lightTheme = {
+  mode: 'light',
   background: '#F5F5F5',
   text: '#222',
   primary: '#555555',
   header: '#FFFFFF',
-}
+  inputBackground: '#FFFFFF',
+} as const
 
 export const darkTheme = {
+  mode: 'dark',
   background: '#1E1E1E',
   text: '#EAEAEA',
   primary: '#2A2A2A',
   header: '#333333',
-}
+  inputBackground: '#333333',
+} as const
