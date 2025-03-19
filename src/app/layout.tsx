9d6ba0a97f335ab25db9c06959aa3c395b8dba00
Patrick Morais <ppternunes@gmail.com>
2025-03-19 18:59:25 -0300

    feat: add homepage and providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { Provider } from '../components/ui/provider'
+import { ThemeProvider } from '../context/ThemeContext'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +8,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Provider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </Provider>
+      </body>
     </html>
   )
 }
