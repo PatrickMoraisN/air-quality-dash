2544fdc3403826f93e94c0f85ae875b457ad8e0c
Patrick Morais <ppternunes@gmail.com>
2025-03-19 16:27:32 -0300

    feat(HomeLayout): create Home layout

---
+++ src/layouts/HomeLayout/HomeLayout.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HomeLayoutContainer = styled.div`
+  gap: 2rem;
+  text-align: center;
+  padding-bottom: 24px;
+`
+
+export const PrincipalText = styled.h2`
+  margin-top: 48px;
+  font-size: 1.5rem;
+  font-style: italic;
+`
---
+++ src/layouts/HomeLayout/HomeLayout.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Dialog } from '@chakra-ui/react'
+import { Header } from '@components/Header'
+import { ListComponent } from '@components/ListComponent'
+import { MapComponent } from '@components/MapComponent'
+import { ViewToggle } from '@components/ViewToggle'
+import { JSX, useState } from 'react'
+import * as S from './HomeLayout.style'
+
+type ViewModeProps = 'map' | 'list'
+
+export function HomeLayout() {
+  const [viewMode, setViewMode] = useState<ViewModeProps>('map')
+
+  const viewModeRenderMap: Record<ViewModeProps, JSX.Element> = {
+    map: <MapComponent />,
+    list: <ListComponent />,
+  }
+
+  return (
+    <Dialog.Root>
+      <S.HomeLayoutContainer>
+        <Header />
+        <S.PrincipalText>Selecione o bairro para saber a qualidade do ar!</S.PrincipalText>
+        <ViewToggle setViewMode={setViewMode} viewMode={viewMode} />
+        {viewModeRenderMap[viewMode]}
+      </S.HomeLayoutContainer>
+    </Dialog.Root>
+  )
+}
