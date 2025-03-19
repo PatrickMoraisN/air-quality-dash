d26d124fb822d05f00450009b40db08027d14d5c
Patrick Morais <ppternunes@gmail.com>
2025-03-19 19:54:22 -0300

    fix(NeighborhoodPopup): fix types and interfaces

--- src/components/NeighborhoodPopup/index.tsx
+++ src/components/NeighborhoodPopup/index.tsx
@@ -1,10 +1,21 @@
 import { Dialog, DialogBody } from '@chakra-ui/react'
 import { AirQualityText } from '@components/AirQualityText'
+import AirQualityTextType from '@utils/airQuality'
 import { AirQualityChart } from '../AirQualityChart'
 import * as S from './styles'
 
+interface QualityHistoryProps {
+  [key: string]: 'bom' | 'moderado' | 'ruim' | 'péssimo'
+}
+
+interface SelectedNeighborhood {
+  name: string
+  actual_quality: string
+  history: QualityHistoryProps
+}
+
 interface NeighborhoodPopupProps {
-  selectedNeighborhood: any
+  selectedNeighborhood: SelectedNeighborhood
   isPopupOpen: boolean
   closeModal: () => void
 }
@@ -26,9 +37,14 @@ export function NeighborhoodPopup({
           <DialogBody>
             <S.DialogHeader>{selectedNeighborhood.name}</S.DialogHeader>
             <S.DialogText>A qualidade do ar atual é:</S.DialogText>
-            <AirQualityText airQuality={selectedNeighborhood.actual_quality} variant="popup" />
+            <AirQualityText
+              airQuality={selectedNeighborhood.actual_quality as AirQualityTextType}
+              variant="popup"
+            />
             <S.RecentMeasurementsText>Medições recentes:</S.RecentMeasurementsText>
-            {selectedNeighborhood && <AirQualityChart history={selectedNeighborhood.history} />}
+            {selectedNeighborhood && (
+              <AirQualityChart history={selectedNeighborhood.history as QualityHistoryProps} />
+            )}
           </DialogBody>
         </S.DialogContent>
       </Dialog.Root>
