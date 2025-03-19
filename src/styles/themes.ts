39a7f6d77bbfc37855038896c39e5dfb3c7dd3f6
Patrick Morais <ppternunes@gmail.com>
2025-03-19 16:28:05 -0300

    feat(themes): add charts theme

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -5,6 +5,13 @@ export const lightTheme = {
   primary: '#555555',
   header: '#FFFFFF',
   inputBackground: '#FFFFFF',
+  tooltipBg: '#FFFFFF',
+  tooltipText: '#000000',
+  chart: {
+    stopOne: '##004a99',
+    stopTwo: '#014996',
+    stopThree: '#489af3',
+  },
 } as const
 
 export const darkTheme = {
@@ -14,4 +21,11 @@ export const darkTheme = {
   primary: '#2A2A2A',
   header: '#333333',
   inputBackground: '#333333',
+  tooltipBg: '#333333',
+  tooltipText: '#FFFFFF',
+  chart: {
+    stopOne: '#FFF',
+    stopTwo: '#FFF',
+    stopThree: '#1E1E1E',
+  },
 } as const
