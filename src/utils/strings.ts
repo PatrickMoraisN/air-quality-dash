e66cd951e2304d2d9a538ae6a9de1a02de65bce6
Patrick Morais <ppternunes@gmail.com>
2025-03-20 14:46:01 -0300

    fix(strings-utils): fix toTitleCase

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -2,6 +2,7 @@ export const toTitleCase = (str: string): string => {
   const lowercaseWords = new Set(['do', 'da', 'de', 'dos', 'das'])
 
   return str
+    .toLowerCase()
     .split(' ')
     .map((word, index) =>
       lowercaseWords.has(word) && index !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
