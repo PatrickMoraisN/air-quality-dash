32ff5ffbd4495547a519dca52ceca924d2dcf8db
Patrick Morais <ppternunes@gmail.com>
2025-03-21 17:14:30 -0300

    feat(constants): create error messagens enum

---
+++ src/constants/errorMessages.ts
@@ -0,0 +1,10 @@
+enum ErrorMessages {
+  FailedToFetchPaginated = 'Não foi possível carregar os dados dos bairros.',
+  FailedToFetchFiltered = 'Não foi possível carregar os dados filtrados.',
+  FailedToSearchByName = 'Erro ao buscar bairro pelo nome.',
+  FailedToFetchTotal = 'Erro ao buscar o total de bairros.',
+  FailedGeoJSONData = 'Erro ao buscar dados geográficos.',
+  GeneralError = 'Erro inesperado. Tente novamente mais tarde.',
+}
+
+export default ErrorMessages
