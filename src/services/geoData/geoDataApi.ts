a5f8e1fdbfc08c412d7e9e98da094573d920331c
Patrick Morais <ppternunes@gmail.com>
2025-03-21 17:15:11 -0300

    feat(geoDataApi): add try/catch blocks

--- src/services/geoData/geoDataApi.ts
+++ src/services/geoData/geoDataApi.ts
@@ -1,12 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
+import ErrorMessages from '@constants/errorMessages'
 import { geoData } from '@utils/geojson/geoData'
 
 const getGeoDataJSON = async () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(geoData)
-    }, 2000)
-  })
+  try {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(geoData)
+      }, 2000)
+    })
+  } catch (error) {
+    console.error(ErrorMessages.FailedGeoJSONData, error)
+    throw new Error(ErrorMessages.FailedGeoJSONData)
+  }
 }
 
 export default {
